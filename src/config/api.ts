10a7e90feba68a85f65de9a97a452996467bf7c9
anacriquintero <ana_cri.quintero@uao.edu.co>
2025-11-18 00:28:52 -0500

    feat: urlfija

--- src/config/api.ts
+++ src/config/api.ts
@@ -4,7 +4,7 @@
  */
 
 // URL del backend - Siempre apunta a producción
-const BACKEND_URL = 'https://cool-symbols-float.loca.lt';
+const BACKEND_URL = 'https://salud-digital-backend.onrender.com/api';
 export const API_BASE_URL = `${BACKEND_URL}/api`;
 
 // Log para debugging
